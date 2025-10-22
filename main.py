394299d90630522759b87b4ef8507b15a2b2e660
Tursunxon <ozodbekova@gmail.com>
2025-10-22 09:18:00 +0500

    yuklash 1

---
+++ main.py
@@ -0,0 +1,18 @@
+import cv2
+import numpy as np
+
+image = cv2.imread('third.jpg')
+cv2.imshow('Original', image)
+
+kernel = np.array([[0, -1, 0],
+                   [-1, 5, -1],
+                   [0, -1, 0]])
+
+sharpened = cv2.filter2D(image, -2, kernel)
+
+cv2.imshow('Natija', sharpened)
+
+cv2.imwrite('Natija.jpg', sharpened)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
